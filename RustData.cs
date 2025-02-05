1e381f71014b40af45f2e02b34938cb3c013d3ea
Felix Kuen <Felixkuen123@hotmail.com>
2025-02-05 12:01:42 +0100

    Auto-update: Compiled DLL

--- RustData.cs
+++ RustData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Carbon;
 using API.Assembly;
 
-    namespace Carbon.Extensions {
+    namespace RustDataExtension {
 
     
 	public class RustData : ICarbonExtension
